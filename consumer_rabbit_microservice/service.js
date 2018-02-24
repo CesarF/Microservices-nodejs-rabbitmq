a1869dbdfe74eec3e53494b3bf863c8a4737b707
César Forero <cesar.forero.gz@gmail.com>
2018-02-24 10:00:32 -0500

    Update service.js

--- consumer_rabbit_microservice/service.js
+++ consumer_rabbit_microservice/service.js
@@ -1,13 +1,13 @@
 var amqp = require('amqplib/callback_api');
 
   //process.env.RABBIT_HOST=localhost:5672
-  amqp.connect('amqp://test:test@10.1.1.113:5672', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var q = 'test';
-    ch.assertQueue(q, {durable: false});
-    ch.consume(q, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
-    }, {noAck: true});
+  amqp.connect('amqp://'+PASSWD+':'+USER+'@'+IP_RABBIT+':'+RABBIT_PORT, function(err, conn) {
+      conn.createChannel(function(err, ch) {
+      var q = 'test';
+      ch.assertQueue(q, {durable: false});
+      ch.consume(q, function(msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+      }, {noAck: true});
       console.log("Connection succesful");
   });
 });
